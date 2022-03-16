b10c844a99d8da5f566a19b7f1b516538ee819a5
PC03 <PC03@INFO5-5374>
2022-03-16 09:44:48 +0100

    aggiungere attributi privati di Articolo

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -10,13 +10,22 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
     {
         // TODO: (8) aggiungere attributi privati di Articolo
         // ...
+        int codice;
+        string descrizione;
+        string unitaMisura;
+        double prezzo;
+
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
         // ...
+
         public Articolo(int codice, string descrizione, string unitaMisura, double prezzo)
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
             // ...
+            this.descrizione = descrizione;
+            this.unitaMisura = unitaMisura;
+            this.prezzo = prezzo;
         }
 
         public string Visualizzati()
