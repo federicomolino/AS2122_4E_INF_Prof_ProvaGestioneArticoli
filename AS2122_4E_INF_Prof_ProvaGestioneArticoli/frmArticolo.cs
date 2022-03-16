a8d7e84f9b88597239bc1ee02a55806ffafe3706
PC03 <PC03@INFO5-5374>
2022-03-16 10:06:28 +0100

    aggiungere attributi privati dei dati inseriti nella frmArticoli

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
@@ -16,6 +16,9 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
 
         // TODO: (5) aggiungere attributi privati dei dati inseriti nella frmArticoli
         // ...
+        private string Descrizione { get; private set; }
+        private string UnitaMisura { get; private set; }
+        private double Prezzo { get; private set; }
 
         public DialogResult Status { get { return status; } }
 
